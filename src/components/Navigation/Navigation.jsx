93a27ba07d08b068b17c3be5c58e4b018ad8ecee
nurealammiaji <nurealam.miaji@gmail.com>
2023-09-06 00:08:32 +0600

    nav bar added

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,40 +1,63 @@
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import { FaBarsStaggered, FaX } from "react-icons/fa6";
+import { useState } from "react";
 
 const Navigation = () => {
 
+    const [navbar, setNavbar] = useState(false);
+
     return (
         <div>
-            <div className="bg-transparent navbar">
-                <div className="flex-1">
-                    <div className="w-44">
-                        <a className="text-xl normal-case btn btn-ghost">
-                            <img src={logo} alt="" />
-                        </a>
-                    </div>
-                </div>
-                <div className="flex-none gap-2">
-                    <div className="form-control">
-                        <input type="text" placeholder="Search" className="w-24 input input-bordered md:w-auto" />
-                    </div>
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <nav className="w-full bg-transparent shadow">
+                <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+                    <div>
+                        <div className="flex items-center justify-between py-3 md:block">
+                            <div className="w-28">
+                                <Link to="/"><img src={logo} alt="" /></Link>
                             </div>
-                        </label>
-                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
+                            <div className="md:hidden">
+                                <button
+                                    className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                    onClick={() => setNavbar(!navbar)}
+                                >
+                                    {navbar ? (
+                                        <FaX />
+                                    ) : (
+                                        <FaBarsStaggered />
+                                    )}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div
+                            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"
+                                }`}
+                        >
+                            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                                <li className="text-gray-600 hover:text-blue-600">
+                                    <div className="form-control">
+                                        <input type="text" placeholder="Search" className="w-auto input input-bordered" />
+                                    </div>
+                                </li>
+                                <li className="text-gray-600 hover:text-blue-600">
+                                    <NavLink to="">News</NavLink>
+                                </li>
+                                <li className="text-gray-600 hover:text-blue-600">
+                                    <NavLink to="/destination">Destination</NavLink>
+                                </li>
+                                <li className="text-gray-600 hover:text-blue-600">
+                                    <NavLink to="/blog">Blog</NavLink>
+                                </li>
+                                <li className="text-gray-600 hover:text-blue-600">
+                                    <NavLink to="/contact">Contact</NavLink>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </nav>
         </div>
     );
 };
