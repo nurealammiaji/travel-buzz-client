3c95bea6d14ba21156dc5b86164e5f1d510daa9c
nurealammiaji <nurealam.miaji@gmail.com>
2023-09-06 04:15:46 +0600

    loader added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,62 @@
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { AuthContext } from "../../Providers/Providers";
+import { FaGoogle, FaGithub } from "react-icons/fa6";
 
 const Login = () => {
+
+    const { login, googleLogin, githubLogin } = useContext(AuthContext);
+    const [displayError, setDisplayError] = useState(null);
+
+    const loginHandler = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        login(email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+        .catch(error => {
+            console.log(error.message);
+            if (error) {
+                setDisplayError(error.message);
+            }
+        })
+    }
+
+    const googleLoginHandler = () => {
+        googleLogin()
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+        .catch(error => {
+            console.log(error.message);
+            setDisplayError(error.message);
+        })
+    }
+
+    const githubLoginHandler = () => {
+        githubLogin()
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+        .catch(error => {
+            console.log(error.message);
+            setDisplayError(error.message);
+        })
+    }
+
     return (
         <div>
             <br /><br />
-            <div className="min-h-screen rounded hero bg-base-200">
-                <div className="flex-col hero-content">
-                    <div className="flex-shrink-0 w-10/12 max-w-sm shadow-2xl md:w-full card bg-base-100">
-                        <div className="card-body">
+            <div className="py-10 rounded hero bg-base-200">
+                <div className="flex">
+                    <div className="flex-shrink-0 w-10/12 max-w-sm mx-auto shadow-2xl md:w-full card bg-base-100">
+                        <form onSubmit={loginHandler} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -30,10 +78,13 @@ const Login = () => {
                                 </div>
                                 <p className="my-3 italic">or</p>
                                 <div className="mt-3 form-control">
-                                <button type="button" className="btn btn-primary">Google Login</button>
+                                    <button onClick={googleLoginHandler} type="button" className="btn btn-primary"><FaGoogle /> Google Login</button>
+                                </div>
+                                <div className="mt-3 form-control">
+                                    <button onClick={githubLoginHandler} type="button" className="btn btn-primary"><FaGithub /> Github Login</button>
+                                </div>
                             </div>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/components/Private/Private.jsx
+++ src/components/Private/Private.jsx
@@ -7,7 +7,13 @@ const Private = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
 
     if (loading) {
-        return <span className="loading loading-ring loading-lg"><h3 className="text-xl">Loading <span className="loading loading-dots loading-xs"></span></h3></span>
+        return <>
+            <br /><br />
+            <span className="loading loading-ring loading-lg"></span>
+            <br /><br />
+            <h3 className="text-2xl">Loading <span className="ml-3 loading loading-dots loading-md"></span></h3>
+
+        </>
     }
 
     if (user) {
